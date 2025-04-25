16b6a4ceacee53ec25c6da88a29a9eb1e7b1cb6f
strelaiznigerii <sergey.kuzminru@yandex.ru>
2025-04-25 14:47:55 +0300

    Add class Cafe

--- cafe.py
+++ cafe.py
@@ -24,7 +24,25 @@ class Order:
         return '\n'.join([str(item) for item in self.items]) + f"\nИтого: {self.total_price()} руб." 
 
 class Cafe:
-    pass
+    def __init__(self) -> None:
+        self.menu = list()
+        self.orders = list()
+
+    def add_menu_item(self, item: MenuItem):
+        self.menu.append(item)
+
+    @staticmethod
+    def create_order() -> Order:
+        order = Order()
+
+    def add_order(self, order: Order) -> None:
+        self.orders.append(order)
+
+    def list_menu(self) -> list:
+        return [item for item in self.menu]
+    
+    def __str__(self) -> str:
+        return '\n'.join(self.list_menu())
 
 a1 = MenuItem('шашлык', 100.00)
 print(a1)
