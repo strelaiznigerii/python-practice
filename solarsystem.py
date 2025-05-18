a3385242852872d0053cdf0b22e324abf519ec85
strelaiznigerii <sergey.kuzminru@yandex.ru>
2025-05-18 19:08:33 +0600

    SolarSystem: some fixes, but not finished

--- solarsystem.py
+++ solarsystem.py
@@ -7,8 +7,8 @@ class Star:
     temperature: float 
 
     def __post_init__(self):
-        if self.mass < 0:
-            raise ValueError("Масса не может быть отрицательной")
+        if self.mass < 0: 
+            raise ValueError('Звезда не может иметь отрицательную массу')
 
     def shine(self) -> str:
        return 'Светит ярко' if self.temperature > 5000 else 'Светит тускло'
@@ -37,9 +37,10 @@ class SolarSystem:
         return f'Планета {planet.name} добавлена'
 
     def display_info(self):
-        
+       pass 
 
 sun = Star("Солнце", 1.989e30, 5778)
+alpha = Star("Альфа Центавра", -12, -404504)
 earth = Planet("Земля", 5.972e24, 149.6e6, 365.25)
 mars = Planet("Марс", 6.39e23, 227.9e6, 687)
 
