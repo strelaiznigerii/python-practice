4f8f3eb061fde368366db23e9581b9f7964d2f97
strelaiznigerii <sergey.kuzminru@yandex.ru>
2025-06-02 08:33:13 +0300

    shop.py: Continue the task

--- shop.py
+++ shop.py
@@ -17,17 +17,21 @@ class CartItem:
     def __str__(self) -> str:
         return f'{self.product}Кол-во: {self.quantity}\n---\n'
 
+@dataclass
 class ShoppingCart:
     shoppingcart: list[CartItem] = field(default_factory=list)
+    sum_of_order: int = 0
 
     def add_cart_item(self, cartitem: CartItem) -> None:
        self.shoppingcart.append(cartitem)
 
-    def delete_cart_item(self, cartitem: CartItem):
-        self.shoppingcart.remove(cartitem)
+    def delete_cart_item(self, cartitem: CartItem) -> None:
+       self.shoppingcart.remove(cartitem)
 
-    def sum_of_order(self):
-        pass
+    def sum_of_order(self) -> int:
+        for item in self.shoppingcart:
+            self.sum_of_order += item.quantity * item.price
+        return f'Сумма заказа: {self.sum_of_order}'
 
 @dataclass
 class Order:
@@ -35,11 +39,12 @@ class Order:
     _order_status: str
 
     @property
-    def order_status(self) -> None:
-        pass
+    def order_status(self) -> str:
+        return self._order_status
+
     @order_status.setter
-    def order_status(self) -> None:
-        pass
+    def order_status(self, status: str) -> None:
+        self._order_status = status
 
     def pay(self):
         pass
@@ -57,3 +62,10 @@ if __name__ == "__main__":
 
     shampoo_item = CartItem(shampoo, 2)
     print(shampoo_item)
+
+    shoppingcart = ShoppingCart()
+    shoppingcart.add_cart_item(shampoo_item)
+    print(shoppingcart) 
+    print(shoppingcart.sum_of_order())
+    shoppingcart.delete_cart_item(shampoo_item)
+    print(shoppingcart)
